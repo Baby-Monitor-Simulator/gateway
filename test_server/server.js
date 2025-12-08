61628a2328dfc3d6df9d913bc00a557c911dc9a0
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2025-12-08 10:01:55 +0100

    update endpoints

--- test_server/server.js
+++ test_server/server.js
@@ -5,21 +5,6 @@ const wss = new WebSocket.Server({ port });
 
 console.log(`WebSocket server running on port ${port}`);
 
-//wss.on('connection', (ws) => {
-//  console.log('Client connected');
-
-//  ws.send('Welcome! Connected to backend via Kong');
-
-//  ws.on('message', (message) => {
-//    console.log('Received:', message.toString());
-//    ws.send(`Echo: ${message}`);
-//  });
-
-//  ws.on('close', () => {
-//    console.log('Client disconnected');
-//  });
-//});
-
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
